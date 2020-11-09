03ac95eaf84a080027d0a2417ab16e7fc68a8220
Christopher Breen <C.jbreen61@gmail.com>
2020-11-09 15:59:58 -0500

    commit tree c2602a5929baa682fe0732adc13f076fb055b975

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,5 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+Greeting = "Hello World!"
+  puts phrase
+end
